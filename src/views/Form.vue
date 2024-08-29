113abcef13f8e6a76780adafba9c233398b2c2ad
JoÃão Gabriel Muniz da Silva <gabrielmunizdasilva40@gmail.com>
2024-08-28 21:31:13 -0300

    feat: criando Form.vue e List.vue

---
+++ src/views/Form.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container mt-2">
+    <h1>Formulário</h1>
+    <b-form>
+      <b-form-group
+      label="Título"
+      label-for="subject"
+      >
+      <b-form-input
+      id="subject"
+      v-model="form.subject"
+      type="text"
+      placeholder="Ex: lavar carro"
+      required
+      autocomplite="off"
+      ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+      label="Descrição"
+      label-for="description"
+      >
+      <b-form-textarea
+      id="description"
+      v-model="form.description"
+      type="text"
+      placeholder="Ex: preciso levar o carro para lavar as 14:00"
+      required
+      autocomplite="off"
+      ></b-form-textarea>
+
+      </b-form-group>
+
+      <b-button type ="submit" variant = "outline-primary">Salvar</b-button>
+
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+
+  data(){
+    return {
+      form : {
+        subject : "",
+        description : ""
+      }
+    }
+  }
+}
+
+</script>
